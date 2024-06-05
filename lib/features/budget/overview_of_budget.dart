22d0180e4c72789953b1648df04e33cf40e26a10
Cheng Jing <chengjing32@yahoo.com>
2024-06-05 15:05:18 +0800

    updated the bottom nav bar to be able to flow between all the temp pages (i.e. home, budget, add, profile)

---
+++ lib/features/budget/overview_of_budget.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class OverviewOfBudget extends StatefulWidget {
+  const OverviewOfBudget({super.key});
+
+  @override
+  _OverviewOfBudgetState createState() => _OverviewOfBudgetState();
+}
+
+class _OverviewOfBudgetState extends State<OverviewOfBudget> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("overview Of Budgets!"),
+    );
+  }
+
+}
