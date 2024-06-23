309d1c58b53976e5ef78fd8264fe757a3393f5fc
Cheng Jing <chengjing32@yahoo.com>
2024-06-23 20:46:51 +0800

    Update: update the user_information.dart file with the logged in user

--- lib/features/authentication/application/auth.dart
+++ lib/features/authentication/application/auth.dart
@@ -1,27 +1,52 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_kakis/features/authentication/application/login.dart';
 import 'package:travel_kakis/home_page.dart';
 import 'package:travel_kakis/main.dart';
+import 'package:travel_kakis/utils/user_information.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:travel_kakis/utils/user_information.dart' as user_info;
+
 
 class Auth extends StatelessWidget {
+
   const Auth({super.key});
+
+  void getData() async{
+
+    final DocumentReference currUserDoc = await FirebaseFirestore.instance.collection('users')
+      .doc(user_info.getID());
+
+    currUserDoc.get().then(
+        (DocumentSnapshot doc) {
+          final data = doc.data() as Map<String, dynamic>;
+          user_info.setUsername(data['name']);
+          user_info.setDesc(data['desc']);
+          user_info.setEmail(data['email']);
+          user_info.setJoinedDate(data['dateJoined']);
+        },
+      onError: (e) => print("Error getting document: $e"),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          //If the User Is Logged In
-          if (snapshot.hasData) {
-            return HomePage();
-          } else {
-            //If the User is not Logged In
-            return Login();
-          }
-        },
-      )
-    );
+      //sticks the bot nav to the bottom of the phone when keyboard appears
+        resizeToAvoidBottomInset: false,
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            //If the User Is Logged In
+            if (snapshot.hasData) {
+              getData();
+              return const HomePage();
+            } else {
+              //If the User is not Logged In
+              return const Login();
+            }
+          },
+        ));
   }
 }
