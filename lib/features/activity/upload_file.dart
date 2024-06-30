2a24e290c970c727f1b7cf054f477c5e0d57ebf0
Cheng Jing <chengjing32@yahoo.com>
2024-07-01 06:27:33 +0800

    FEAT: working on the upload_file

---
+++ lib/features/activity/upload_file.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
+
+class UploadFile extends StatefulWidget {
+ 
+  const UploadFile({
+    super.key,
+  });
+
+  @override
+  _UploadFileState createState() => _UploadFileState();
+}
+
+class _UploadFileState extends State<UploadFile> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return Center(
+      child: Text('Hello'),
+    );
+  }
+  
+  
+}
